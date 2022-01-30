33b16dbc6e3ae9aa35071d3becaf742bf5d50054
StrvKudos <sag21002@byui.edu>
2022-01-30 14:11:40 +0800

    G

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    document.getElementById("sally").classList.toggle("open");
+    document.getElementById("hamburgerbtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerbtn');
+x.onlick = toggleMenu;
